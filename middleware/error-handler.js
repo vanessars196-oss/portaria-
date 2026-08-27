f9618ca739f80860d905d5060481dc695683f7b5
ITNEROL-Sistema comercial <vanessa.rs196@gmail.com>
2026-08-27 12:49:55 -0300

    Create error-handler.js

---
+++ middleware/error-handler.js
@@ -0,0 +1,11 @@
+module.exports = (err, req, res, next) => {
+  console.error(err);
+
+  const status = err.status || 500;
+  const message = err.message || 'Erro interno do servidor';
+
+  res.status(status).json({
+    success: false,
+    message
+  });
+};
